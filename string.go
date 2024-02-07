93e49d88f938a4abcdbd57a41fc20595c87a7848
NotAstronot <feririski409@gmail.com>
2024-02-07 12:46:53 +0700

    update

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Feri")
+	fmt.Println("Riski")
+	fmt.Println("Muh Feri Riski")
+
+	fmt.Println(len("Feri"))
+	fmt.Println("Riski"[2])
+	fmt.Println("Muh Feri Riski"[1])
+}
