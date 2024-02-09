f8d8e70ce1c7bd0df9c5a3b49209dff1e20466c2
NotAstronot <feririski409@gmail.com>
2024-02-09 14:36:50 +0700

    update

---
+++ operasi_matematika.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = 50
+	var b = 3
+	var c = a / b
+	fmt.Println(c)
+
+	var i = 10
+	i += 10
+	i++
+	fmt.Println(i)
+
+	var j = 20
+	j++
+	fmt.Println(j)
+
+	// Jika dikurangkan
+	j--
+	fmt.Println(j)
+}
+
+/* (+) adalah penjumlahan
+(-) adalah pengurangan
+(*) adalah perkalian
+(/) adalah pembagian
+(%) adalah sisa pembagian atau modulo*/
